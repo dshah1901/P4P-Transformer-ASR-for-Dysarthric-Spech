06f275a839b3c58996fe85451e989327e511f593
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-09-12 10:15:59 +1200

    Froze Token Embeddings

--- main.py
+++ main.py
@@ -467,7 +467,7 @@ class DisplayOutputs(keras.callbacks.Callback):
 
             print('{} score of one validation batch: {:.2f}\n'.format("WER", float(wer(target_text, prediction))))
 
-            self.model.save_weights(f'LJ200_2.0_UAControl_FreezeE1.h5')
+            self.model.save_weights(f'LJ200_2.0_UAControl_FreezeTokenEmbeddings.h5')
         print('{} total score of one validation batch: {:.2f}\n'.format("WER", (score)/float(bs)))
         data = pd.DataFrame({"A":epoch,"B":(score)/float(bs)}, index=[0])
         with pd.ExcelWriter("Epoch Accuracy.xlsx",mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer:
@@ -587,10 +587,14 @@ model.summary();
 # for layers in (model.layers)[2]:
 #     print(layers)
 #     layers.trainable = False
-# print((model.layers)[2])
-#((model.layers)[2]).trainable = False
-#((model.layers)[3]).trainable = False
-((model.encoder.layers)[1]).trainable = False;
+# print((model.layers)[1]) #Token embeddings
+# print((model.layers)[2]) #Sequential
+# print((model.layers)[3]) #Transformer decoder
+# print((model.layers)[4]) #Transformer decoder
+# print((model.layers)[5]) #Transformer decoder
+# print((model.layers)[6]) #dense
+((model.layers)[1]).trainable = False
+
 # ((model.encoder.layers)[2]).trainable = False;
 # ((model.encoder.layers)[3]).trainable = False;
 # ((model.encoder.layers)[4]).trainable = False;
