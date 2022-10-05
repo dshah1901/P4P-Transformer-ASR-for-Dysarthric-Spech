4083bf927a033a06bf1528610d961dc0b4cf0754
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-10-05 21:57:29 +1300

    M09 Froze Token Embeddings Data Aug

--- main.py
+++ main.py
@@ -481,7 +481,7 @@ class DisplayOutputs(keras.callbacks.Callback):
 
             print('{} score of one validation batch: {:.2f}\n'.format("WER", float(wer(target_text, prediction))))
 
-            self.model.save_weights(f'Augmented_M09_D3.h5')
+            self.model.save_weights(f'Augmented_M09_TE.h5')
         print('{} total score of one validation batch: {:.2f}\n'.format("WER", (score)/float(bs)))
         data = pd.DataFrame({"A":epoch,"B":(score)/float(bs)}, index=[0])
         with pd.ExcelWriter("Epoch Accuracy.xlsx",mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer:
@@ -612,10 +612,10 @@ model.summary();
 # ((model.encoder.layers)[4]).trainable = False;
 # ((model.encoder.layers)[5]).trainable = False;
 
-# ((model.layers)[1]).trainable = False
+((model.layers)[1]).trainable = False
 # ((model.layers)[3]).trainable = False
 # ((model.layers)[4]).trainable = False
-((model.layers)[5]).trainable = False
+# ((model.layers)[5]).trainable = False
 # ((model.layers)[6]).trainable = False
 
 model.summary(); 
