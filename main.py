92febcdd391594d528003bec0788041e1c7024a3
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-10-06 00:54:48 +1300

    F04 Froze Data Aug TE+D1

--- main.py
+++ main.py
@@ -481,7 +481,7 @@ class DisplayOutputs(keras.callbacks.Callback):
 
             print('{} score of one validation batch: {:.2f}\n'.format("WER", float(wer(target_text, prediction))))
 
-            self.model.save_weights(f'Augmented_F04_TE.h5')
+            self.model.save_weights(f'Augmented_F04_TE+D1.h5')
         print('{} total score of one validation batch: {:.2f}\n'.format("WER", (score)/float(bs)))
         data = pd.DataFrame({"A":epoch,"B":(score)/float(bs)}, index=[0])
         with pd.ExcelWriter("Epoch Accuracy.xlsx",mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer:
@@ -613,7 +613,7 @@ model.summary();
 # ((model.encoder.layers)[5]).trainable = False;
 
 ((model.layers)[1]).trainable = False
-# ((model.layers)[3]).trainable = False
+((model.layers)[3]).trainable = False
 # ((model.layers)[4]).trainable = False
 # ((model.layers)[5]).trainable = False
 # ((model.layers)[6]).trainable = False
